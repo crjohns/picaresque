67c40eb66c99398aa7f8c1e9afa5510648acf250
Chris Johnson <crjohns@mit.edu>
2012-07-24 00:21:33 -0400

    Updated game to remove opengl stuff

--- src/game.py
+++ src/game.py
@@ -4,34 +4,24 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+class InvalidType:
+    pass
 
 class Game:
+
+    renderList = []
 
     def __init__(self, dimensions=(800,600)):
         pygame.init()
         self.dimensions=dimensions
-        self.screen = pygame.display.set_mode(dimensions, OPENGL|HWSURFACE|DOUBLEBUF)
+        self.screen = pygame.display.set_mode(dimensions, HWSURFACE|DOUBLEBUF)
 
         self.clock = pygame.time.Clock()
-        print self.clock
 
-        glViewport(0,0,dimensions[0], dimensions[1])
-        glClearColor(1.,0.,0.,1.)
-        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL) 
-
-        glMatrixMode(GL_PROJECTION)
-        gluOrtho2D(0, 100, 0, 100)
-
-    def render(self):
-        glClear(GL_COLOR_BUFFER_BIT)
-
-        glBegin(GL_TRIANGLES)
-        glColor3f(1., 1., 0.)
-        glVertex2f(10., 10.)
-        glVertex2f(40., 10.)
-        glVertex2f(40., 40.)
-        glEnd()
-
+    def addRenderable(self, obj):
+        if not obj.render:
+            raise InvalidType
+        renderList.append(obj)
 
     def mainLoop(self):
         while 1:
@@ -43,7 +33,6 @@ class Game:
                     sys.exit()
                  
 
-            self.render()
             pygame.display.flip()
 
             self.clock.tick(60)
